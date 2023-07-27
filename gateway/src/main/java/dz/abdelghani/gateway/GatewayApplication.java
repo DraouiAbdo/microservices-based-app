4d7d209c9b4d9ccda63e6a85e5e1f4e6000d8b89
abdelghani draoui <draoui.abdelghani@gmail.com>
2023-07-27 08:17:37 +0200

    added eureka discovery service to the project, names of microservices specified instead of specifying ip and port => load balancer activated

--- gateway/src/main/java/dz/abdelghani/gateway/GatewayApplication.java
+++ gateway/src/main/java/dz/abdelghani/gateway/GatewayApplication.java
@@ -16,8 +16,8 @@ public class GatewayApplication {
 @Bean
 	RouteLocator routeLocator(RouteLocatorBuilder builder){
 		return builder.routes()
-				.route((r)->r.path("/customers/**").uri("http://localhost:8181/"))
-				.route((r)->r.path("/products/**").uri("http://localhost:8282/"))
+				.route((r)->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE/"))
+				.route((r)->r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
 				.build();
 	}
 
